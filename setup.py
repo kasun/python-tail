777516b98bae0dd9f2c1043a6f65e9af8fa6bd47
Kasun Herath <kasunh01@gmail.com>
2011-12-25 20:45:25 +0530

    Setup script added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-tail',
+    description='Unix Tail follow implementation',
+    author='Kasun Herath',
+    author_email='kasunh01@gmail.com',
+    py_modules=['tail'],
+    )
+
+
