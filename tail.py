48d65e24e4eb24de71968afcb3fe7254d53ee721
Kasun Herath <kasunh01@gmail.com>
2011-12-26 12:47:42 +0530

    Module docstring modified to better explain the module and an example is added.

--- tail.py
+++ tail.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 
 '''
-Python-Tail - Unix tail follow implementation in Python <https://github.com/kasun/python-tail>
-Author - Kasun Herath <kasunh01 at gmail.com>
-'''
+Python-Tail - Unix tail follow implementation in Python. 
+
+python-tail can be used to monitor changes to a file.
+
+Example:
+    import tail
+
+    # Create a tail instance
+    t = tail.Tail('file-to-be-followed')
+
+    # Register a callback function to be called when a new line is found in the followed file. 
+    # If no callback function is registerd, new lines would be printed to standard out.
+    t.register_callback(callback_function)
+
+    # Follow the file with 5 seconds as sleep time between iterations. 
+    # If sleep time is not provided 1 second is used as the default time.
+    t.follow(s=5) '''
+
+# Author - Kasun Herath <kasunh01 at gmail.com>
+# Source - https://github.com/kasun/python-tail
 
 import os
 import sys
