c264fb5f403659df861e13e99687fd44c1b7f1b4
Domingo Dirutigliano <dirutigliano20@gmail.com>
2020-09-15 18:55:30 +0200

    Adding print last lines
    Using the new paramether in the follow function, permitt to print last lines choosed on function call for see previous lines (as in tail -f on UNIX)

--- tail.py
+++ tail.py
@@ -19,7 +19,7 @@ Example:
     # If sleep time is not provided 1 second is used as the default time.
     t.follow(s=5) '''
 
-# Author - Kasun Herath <kasunh01 at gmail.com>
+# Author - Kasun Herath <kasunh01@gmail.com>
 # Source - https://github.com/kasun/python-tail
 
 import os
@@ -39,16 +39,35 @@ class Tail(object):
         self.tailed_file = tailed_file
         self.callback = sys.stdout.write
 
-    def follow(self, s=1):
+    def follow(self,p=0, s=1):
         ''' Do a tail follow. If a callback function is registered it is called with every new line. 
         Else printed to standard out.
     
         Arguments:
+            p - Number of line to print before follow; Defaults to 0.
             s - Number of seconds to wait between each iteration; Defaults to 1. '''
 
         with open(self.tailed_file) as file_:
-            # Go to the end of file
-            file_.seek(0,2)
+            
+            if p > 0:
+                #Read lines and split into an array
+                data = file_.read().split('\n')
+                
+                #Skip Empty last line
+                if len(data) > 0:
+                    if data[-1] == '':
+                        data.pop(-1)
+
+                #If the file haven't too much line print all possible printable lines
+                line_to_print = p
+                if len(data) < line_to_print:line_to_print = len(data)
+                #Printing last line
+                for i in reversed(range(1,line_to_print+1)):
+                    print(data[-i])
+            else:
+                # Go to the end of file
+                file_.seek(0,2)
+
             while True:
                 curr_position = file_.tell()
                 line = file_.readline()
